f429a5dd85a06d8eca54f907b89a9dcdcdbd296f
AayushDeus123 <aayushdeus312@gmail.com>
2025-01-26 16:47:59 +0530

    Add files via upload

---
+++ Homework (Class 19).py
@@ -0,0 +1,10 @@
+import math
+a = int(input('Enter the angle to find its Trigonometric values : '))
+
+s = math.sin(a)
+c = math.cos(a)
+t = math.tan(a)
+
+print('\nThe Sine of' ,a, 'is' ,s)
+print('The Cosine of' ,a, 'is' ,c)
+print('The Tangent of' ,a, 'is' ,t)
